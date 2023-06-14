6533b104743dccd70fc32715f5de72260559efab
Jack Caulfield <42452695+jackcauliflower@users.noreply.github.com>
2023-06-14 15:31:16 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "cp_cores" {
+  type = number
+  description = "The number of cores for the control plane machine(s)"
+  default = 2
+}
+
+variable "cp_memory" {
+  type = number
+  description = "The amount of memory for the control plane machine(s)"
+  default = 4096
+}
+
+variable "cp_disk_size" {
+  type = number
+  description = "The amount of disk space in GBs for the control plane machine(s)"
+  default = 40
+}
+
 variable "machine_name" {
   type = string
   description = "The name of the machines in the cluster, will be used when naming nodes"
@@ -7,4 +25,22 @@ variable "machine_name" {
 variable "username" {
   type = string 
   description = "The SSH username"
+}
+  
+variable "worker_cores" {
+  type = number
+  description = "The number of cores for the worker machine(s)"
+  default = 2
+}
+
+variable "worker_memory" {
+  type = number
+  description = "The amount of memory for the worker machine(s)"
+  default = 4096
+}
+
+variable "worker_disk_size" {
+  type = number
+  description = "The amount of disk space in GBs for the worker machine(s)"
+  default = 40
 }
