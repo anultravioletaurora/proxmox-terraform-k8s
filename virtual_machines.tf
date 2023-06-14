45a9af829eb29a9642eb3607bcac9617867f4d06
Jack Caulfield <42452695+jackcauliflower@users.noreply.github.com>
2023-06-14 10:40:27 -0500

    Update virtual_machines.tf
    adding stuff that probably doesn't work but will configure k3s on the terraformed machines using k3sup

--- virtual_machines.tf
+++ virtual_machines.tf
@@ -44,6 +44,79 @@ resource "proxmox_virtual_environment_vm" "k8s_cp_01" {
       }
     }
   }
+  
+  provisioner "local-exec" {
+    command = <<EOT
+            k3sup install \
+            --ip ${self.network_interface[0].access_config[0].nat_ip} \
+            --cluster
+            --context k3s \
+            --ssh-key ~/.ssh/google_compute_engine \
+            --user $(whoami) \
+            # --k3s-extra-args '--no-deploy -traefik'
+        EOT
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "k8s_cp_01" {
+  name        = "k8s-cp-01"
+  description = "Managed by Terraform"
+  tags        = ["terraform"]
+  node_name   = "<your proxmox node>"
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 6144
+  }
+
+  agent {
+    enabled = true
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  disk {
+    datastore_id = "local-zfs"
+    file_id      = proxmox_virtual_environment_file.debian_cloud_image.id
+    interface    = "scsi0"
+    size         = 32
+  }
+
+  serial_device {} # The Debian cloud image expects a serial port to be present
+
+  operating_system {
+    type = "l26" # Linux Kernel 2.6 - 5.X.
+  }
+
+  initialization {
+    
+    datastore_id      = "local-lvm"
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+    
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+  
+  provisioner "local-exec" {
+    command = <<EOT
+            k3sup install \
+            --ip ${self.network_interface[0].access_config[0].nat_ip} \
+            --server
+            --server-ip ${google_compute_instance.k3s_master_instance.network_interface[0].access_config[0].nat_ip} \
+            --context k3s \
+            --ssh-key ~/.ssh/google_compute_engine \
+            --user $(whoami) \
+            # --k3s-extra-args '--no-deploy -traefik'
+        EOT
+  }
 }
 
 resource "proxmox_virtual_environment_vm" "k8s_worker_01" {
@@ -91,5 +164,15 @@ resource "proxmox_virtual_environment_vm" "k8s_worker_01" {
         address = "dhcp"
       }
     }
+  }
+  
+  provisioner "local-exec" {
+    command = <<EOT
+            k3sup join \
+            --ip ${self.network_interface[0].access_config[0].nat_ip} \
+            --server-ip ${google_compute_instance.k3s_master_instance.network_interface[0].access_config[0].nat_ip} \
+            --ssh-key ~/.ssh/google_compute_engine \
+            --user $(whoami)
+        EOT
   }
 }
